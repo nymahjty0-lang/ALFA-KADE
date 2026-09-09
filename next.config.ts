9c97ae45bbbe2b00739d9c4f026a782504003b23
nymahjty0-lang <nymahjty0@gmail.com>
2026-09-09 15:53:53 +0330

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: { remotePatterns: [{ protocol: "https", hostname: "**" }] }
+  output: "export",
+
+  images: {
+    unoptimized: true,
+  },
+
+  trailingSlash: true,
+
+  basePath: "/ALFA-KADE",
 };
 
 export default nextConfig;
