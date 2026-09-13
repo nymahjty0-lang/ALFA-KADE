f90d51124e1fb15aa010cdbde03e52b3bba545d8
nymahjty0-lang <nymahjty0@gmail.com>
2026-09-13 19:04:06 +0330

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,18 +4,9 @@ const config: Config = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./lib/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        alfa: {
-          black: "#070707",
-          gold: "#d8aa4d",
-          darkGold: "#8f6f2d",
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
